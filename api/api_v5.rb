f2e3c7e9470ba93887cebb0862172e6842b076fc
dblock <dblock@dblock.org>
2012-07-30 21:06:38 -0400

    Fixed typo.

--- api/api_v5.rb
+++ api/api_v5.rb
@@ -3,7 +3,7 @@ require 'api_v4_decorator'
 module Acme
   class API_v5 < Grape::API
     version 'v5', :using => :path, :vendor => 'acme', :format => :json
-    desc "Cretes a spline that can be reticulated."
+    desc "Creates a spline that can be reticulated."
     resource :spline do
 	  post do
 	  	{ :reticulated => params[:reticulated] }
