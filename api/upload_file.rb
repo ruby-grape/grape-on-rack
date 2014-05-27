b86aa1a68ed546d3f32d4826fe532da20dae79b8
dblock <dblock@dblock.org>
2014-05-27 09:49:33 -0400

    Override env['api.format'] to return arbitrary types.

--- api/upload_file.rb
+++ api/upload_file.rb
@@ -2,9 +2,6 @@ module Acme
   class UploadFile < Grape::API
     format :json
 
-    content_type :png, "application/png"
-    content_type :rb, "application/x-ruby"
-
     desc "Upload an image."
     post "avatar" do
       {
@@ -17,6 +14,7 @@ module Acme
     post "download" do
       filename = params[:file][:filename]
       content_type MIME::Types.type_for(filename)[0].to_s
+      env['api.format'] = :binary
       header "Content-Disposition", "attachment; filename*=UTF-8''#{URI.escape(filename)}"
       params[:file][:tempfile].read
     end
